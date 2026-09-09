02b959ce4a59283cd8cec40cc702488e6b1283bd
Bala Vardhan Kumar Doupati <dbvardhankumar@gmail.com>
2026-09-09 22:57:33 +0530

    Refactor NLTK downloader to use a data directory
    Refactor NLTK downloader to create a data directory and download resources to it.

--- backend/nltk_downloader.py
+++ backend/nltk_downloader.py
@@ -1,5 +1,34 @@
+import os
 import nltk
-nltk.download('stopwords')
-nltk.download('punkt')
-nltk.download('punkt_tab')
-nltk.download('wordnet')
+
+BASE_DIR = os.path.dirname(
+    os.path.abspath(__file__)
+)
+
+NLTK_DATA_DIR = os.path.join(
+    BASE_DIR,
+    "nltk_data"
+)
+
+os.makedirs(
+    NLTK_DATA_DIR,
+    exist_ok=True
+)
+
+resources = [
+    "stopwords",
+    "punkt",
+    "punkt_tab",
+    "wordnet"
+]
+
+for resource in resources:
+
+    print(f"Downloading {resource}...")
+
+    nltk.download(
+        resource,
+        download_dir=NLTK_DATA_DIR
+    )
+
+print("NLTK resources downloaded successfully.")
